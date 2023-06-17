41721c71c753e1f59ac5b50db610fb2c2a82043d
Richard Ogoma <richard.ogoma@outlook.com>
2023-06-17 05:21:00 +0100

    Revert "Resolve discontinuous timestamp issue"

--- setup_etl.sh
+++ setup_etl.sh
@@ -13,41 +13,8 @@ mkdir -p data/processed
 # Run "make all" command to set up dependencies and other tasks
 make all
 
-# Calculate the delay until the start of the next minute
-current_seconds=$(date +%s)
-next_minute_seconds=$(( (current_seconds / 60 + 1) * 60 ))
-delay_seconds=$((next_minute_seconds - current_seconds))
-
-# Get the current timestamp
-timestamp=$(date +"%Y-%m-%d %H:%M:%S")
-
-# Echo the sleep duration, timestamp, and reason to console
-echo "[$timestamp] Sleeping for $delay_seconds seconds before starting the ETL pipeline to sync with the data source refresh schedule."
-
-# Define the characters for the rolling cursor animation
-cursor_chars=("◐" "◓" "◑" "◒")
-
-# Calculate the duration for each frame
-frame_duration=$((delay_seconds / ${#cursor_chars[@]}))
-
-# Sleep until the start of the next minute
-for ((i = 0; i < delay_seconds; i++)); do
-  # Calculate the index of the current cursor character
-  animation_index=$((i / frame_duration % ${#cursor_chars[@]}))
-
-  # Print the current cursor character and timestamp
-  timestamp=$(date +"%Y-%m-%d %H:%M:%S")
-  echo -ne "[$timestamp] ${cursor_chars[animation_index]}\r"
-
-  # Sleep for 1 second
-  sleep 1
-done
-
-# Start the ETL pipeline in the background and append stdout to output.log
-nohup nice -n 10 python3 -u etl_pipeline.py >> output.log 2>&1 &
-
-# Echo message before the sleep command
-echo "Waiting for a few seconds to allow the process to start..."
+# Start the ETL pipeline in the background with nohup
+nohup nice -n 10 python3 -u etl_pipeline.py > output.log 2>&1 &
 
 # Sleep for a few seconds to allow the process to start
 sleep 5
